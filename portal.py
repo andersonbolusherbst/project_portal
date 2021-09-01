ef4da042aff89efadd218d924a404bf340c4ab84
andersondbk <32954765+andersondbk@users.noreply.github.com>
2021-09-01 14:17:51 +0700

    Update portal.py

--- portal.py
+++ portal.py
@@ -16,11 +16,11 @@ option = st.sidebar.selectbox("Please select a project type", ('Start Here','Mac
 st.header("You are viewing: "+option)
 
 if option == "Start Here":
-    st.subheader(option)
+    
     st.write("Use the side bar to navigate this portal")
 
 if option == "Machine Learning - Prediction":
-    st.subheader(option)
+    
     project_info = {"Project Name":" Boston 1970s House Price Prediction","Project Type":"Machine Learning - Prediction","Machine Learning Model": "Regression","Machine Learning description":"Resgression models can be used to predcition and forecast based on historical data. Various models can be used for linear, non-linear and logistic relationships. Independant variables (explanatory variables) and used to predict the dependant variable (explained or response variable","Applications":"Marketing, Operations, Logistics, Finance"}
     st.write(project_info)
     project_link ='[GENERATE PROJECT](https://share.streamlit.io/andersonbolusherbst/house_price_prediction/main/boston.py)'
@@ -28,14 +28,14 @@ if option == "Machine Learning - Prediction":
     
 
 if option == "Machine Learning - Classification":
-    st.subheader(option)
+    
     project_info = {"Project Name":" Predicting Subscription Service Cancellations","Project Type":"Machine Learning - Classification","Machine Learning Model": "Decision Tree","Machine Learning description":"Decision Tree models can be used to place dependant variables into groups or categories based on their characteristics/features, in this case - 'cancel' or 'don't cancel' the subscription. There are a variety of classification models that are best suited for different applications and data sets, such as Random Forest, KNN and Support Vector Machine","Applications":"Marketing, Operations, Logistics, Finance"}
     st.write(project_info)
     project_link ='[GENERATE PROJECT](https://share.streamlit.io/andersonbolusherbst/churn/main/churn.py)'
     st.markdown(project_link, unsafe_allow_html=True)
 
 if option == "API Integration":
-    st.header(option)
+    
     if option == 'API Integration':
         st.write("This is live data coming to you from another database. We do this using API's and can connect multiple API's for you, bringing everything you need to one place!")
         st.subheader("_Check out the side bar for options under 'Select Information'!_")
@@ -118,8 +118,8 @@ if option == "API Integration":
 
     
 if option == "Data Analysis":
-    st.subheader(option)
+    
 
 if option == "Automation":
-    st.subheader(option)
+    
 
